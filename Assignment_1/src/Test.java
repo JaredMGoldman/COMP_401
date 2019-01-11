748bcb81b321c612f153f3c087180a9fdf790a36
Jared <jaredgoldman77@gmail.com>
2019-01-11 08:36:16 -0500

    Test commit
    This is a test

---
+++ Assignment_1/src/Test.java
@@ -0,0 +1,9 @@
+public class Test {
+    public static void maybeThisWillWork(){
+        System.out.println("Who knows anymore?");
+    }
+    public static void main(String[]args){
+        maybeThisWillWork();
+    }
+
+}
